6489c2cc00c258a625ad0ee64769e9aa24ce2dbd
marieakoto <marie-pearl.akoto@amalitech.org>
2024-04-02 16:19:20 +0000

    database connection

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -88,9 +88,18 @@ WSGI_APPLICATION = 'todolistapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Todolist',
+        'USER': 'postgres',
+        'PASSWORD': 'k0liko_1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
